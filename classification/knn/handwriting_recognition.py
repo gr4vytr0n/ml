42252cd9081ca90d475ac6f785bc36e2fb65ea42
gr4vytron <33167187+gr4vytr0n@users.noreply.github.com>
2017-11-22 18:33:06 -0800

    handwriting_recognition.py running but seems to be producing the wrong results

--- classification/knn/handwriting_recognition.py
+++ classification/knn/handwriting_recognition.py
@@ -1,4 +1,5 @@
 from os import listdir, chdir, getcwd
+from numpy import zeros, array
 
 from knn import classify0
 
@@ -20,8 +21,41 @@ def get_filenames(directories):
     filenames.append(listdir(directory))
   return filenames
 
+def prep_data(data):
+  vectors = zeros((len(data), 1024))
+  for item in data:
+    lines = item.strip().split('\n')
+    for i in range(32):
+      line = lines[i]
+      vector = zeros((1, 1024))
+      for j in range(32):
+        vector[0, 32*i+j] = int(line[j])
+      vectors[i, :] = vector
+  return vectors
+
+def classifiyHandwriting(testData, trainingData, filenames):
+  testLabels = []
+  trainingLabels = []
+  for filename in filenames[0]:
+    label = int(filename.split('.')[0].split('_')[0])
+    testLabels.append(label)
+  for filename in filenames[1]:
+    label = int(filename.split('.')[0].split('_')[0])
+    trainingLabels.append(label)
+  errorCount = 0.0
+  for test in range(len(testData)):
+    print(testData[test])
+    #classifierResult = classify0(testData[test], trainingData, trainingLabels, 3)
+    print('classifier result: {}, real answer: {}'.format(classifierResult, testLabels[test]))
+    if (classifierResult != testLabels[test]):
+      errorCount += 1.0
+  print('total errors: {}'.format(errorCount))
+  print('total error rate: {}'.format(errorCount/float(len(testData))))
+
+
 if __name__ == '__main__':
   directories = ['testDigits', 'trainingDigits']
   filenames = get_filenames(directories)
-  testData = read_files(directories[0], filenames[0])
-  trainingData = read_files(directories[1], filenames[1])
+  testData = prep_data(read_files(directories[0], filenames[0]))
+  trainingData = prep_data(read_files(directories[1], filenames[1]))
+  classifiyHandwriting(testData, trainingData, filenames)
