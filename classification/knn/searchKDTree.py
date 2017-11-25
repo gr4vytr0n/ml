a350cafaf3b679854db6b5e5cb22209aa81eee8e
gr4vytron <33167187+gr4vytr0n@users.noreply.github.com>
2017-11-25 08:48:12 -0800

    all datasets loaded and processed, test data running fine, classifier working as expected

--- classification/knn/searchKDTree.py
+++ classification/knn/searchKDTree.py
@@ -1,13 +1,22 @@
+import numpy as np
+
 from kdtree import kdtree
+from knn import classify0
 from preprocess_data import preprocess_data
 from normalize import normalize
+
+labelCategories = ['not at all', 'in small doses', 'in large doses']
 
 matrix, labels, categories = preprocess_data('datingTestSet.txt')
 normalized_matrix, ranges, min_vals, max_vals  = normalize(matrix)
 
 tree = kdtree(normalized_matrix)
+practiceTree = kdtree(np.array([(2,3), (5,4), (9,6), (4,7), (8,1), (7,2)]))
 
-def searchTree(tree):
-  return 'search tree'
+searchParameters = [34.0, 4400.0, 0.3]
 
-print(searchTree(tree))
+def searchTree(tree, parameters):
+  return tree
+
+print(searchTree(practiceTree, searchParameters))
+print(labelCategories[classify0(searchParameters, normalized_matrix, labels, 3)])
