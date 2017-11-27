e6e73e7653e88dc23b42aae529a84ca580c3b500
gr4vytron <33167187+gr4vytr0n@users.noreply.github.com>
2017-11-26 20:11:28 -0800

    refactor dating_data.py to work with refactored utils code

--- classification/knn/dating_data/dating_data.py
+++ classification/knn/dating_data/dating_data.py
@@ -1,38 +1,50 @@
-from preprocess_data import preprocess_data
-from normalize import normalize
-from plot_data import plot_data
-from knn import classify0
+#from plot_data import plot_data
 from numpy import array
+from sys import path
+from os import getcwd
 
-matrix, labels, categories = preprocess_data('datingTestSet.txt')
+path.insert(0, '/media/gtron/files/ml/mlclone/classification/knn/utils')
+path.insert(0, '/media/gtron/files/ml/mlclone/classification/knn')
+from preprocess_data import preprocess_data
+from knn import classify0
 
-normalized_matrix, ranges, min_vals, max_vals  = normalize(matrix)
+
+n_array, labels, label_indices = preprocess_data(
+    getcwd() + '/dating_data', 'datingTestSet.txt')
+
 
 #plot_data(normalized_matrix, label_categories, categories)
 
-def test():
-  ho_ratio = 0.10 # 10% of data for test data
-  m = normalized_matrix.shape[0]
-  num_test_vectors = int(m * ho_ratio)
-  error_count = 0.0
-  for i in range(num_test_vectors):
-    classifier_results = classify0(normalized_matrix[i, :], normalized_matrix[num_test_vectors:m, :], \
-      labels[num_test_vectors:m], 3)
-    print('classifier answer: %d, real answer: %d' % (classifier_results, labels[i]))
-    if (classifier_results != labels[i]):
-      error_count += 1.0
-  print('total error rate: %f' % (error_count / float(num_test_vectors)))    
 
-#test()
+def test():
+    ho_ratio = 0.10  # 10% of data for tesing
+    m = n_array.shape[0]
+    num_test_vectors = int(m * ho_ratio)
+    error_count = 0.0
+    for i in range(num_test_vectors):
+        classifier_results = classify0(n_array[i, :], n_array[num_test_vectors:m, :],
+                                       label_indices[num_test_vectors:], 3)
+        print('classifier answer: {}, real answer: {}'.format(labels[classifier_results], labels[label_indices[i]]))
+        if (labels[classifier_results] != labels[label_indices[i]]):
+            error_count += 1.0
+    print('total error rate: {}'.format(error_count / float(num_test_vectors)))
+
+
+test()
 
 # Classify a person with classifier
-def classifyPerson():
-  resultList = ['not at all', 'in small doses', 'in large doses']
-  gaming = float(input('percent time playing video games?'))
-  flyerMiles = float(input('frequent flyer miles earned each year?'))
-  iceCream = float(input('liters of ice cream consumed per year?'))
-  inArr = array([flyerMiles, gaming, iceCream])
-  classifierResult = classify0((inArr-min_vals)/ranges, normalized_matrix, labels, 3)
-  print('You will probably like this person: ', resultList[classifierResult - 1])
 
-classifyPerson()
+
+def classifyPerson():
+    resultList = ['not at all', 'in small doses', 'in large doses']
+    gaming = float(input('percent time playing video games?'))
+    flyerMiles = float(input('frequent flyer miles earned each year?'))
+    iceCream = float(input('liters of ice cream consumed per year?'))
+    inArr = array([flyerMiles, gaming, iceCream])
+    classifierResult = classify0(
+        (inArr - min_vals) / ranges, normalized_matrix, labels, 3)
+    print('You will probably like this person: ',
+          resultList[classifierResult - 1])
+
+
+# classifyPerson()
--- classification/knn/knn.py
+++ classification/knn/knn.py
@@ -1,9 +1,10 @@
-from numpy import *
+from numpy import tile
 import operator
+
 
 def classify0(inX, dataset, labels, k):
     datasetSize = dataset.shape[0]
-    diffMat = tile(inX , (datasetSize, 1)) - dataset
+    diffMat = tile(inX, (datasetSize, 1)) - dataset
     sqDiffMat = diffMat**2
     squareDistances = sqDiffMat.sum(axis=1)
     distances = squareDistances**0.5
@@ -12,7 +13,8 @@ def classify0(inX, dataset, labels, k):
     classCount = {}
     for i in range(k):
         voteLabel = labels[sortedDistances[i]]
-        classCount[voteLabel] = classCount.get(voteLabel,0) + 1
-    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
-    
+        classCount[voteLabel] = classCount.get(voteLabel, 0) + 1
+    sortedClassCount = sorted(
+        classCount.items(), key=operator.itemgetter(1), reverse=True)
+
     return sortedClassCount[0][0]
