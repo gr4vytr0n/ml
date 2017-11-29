e31f3aa835d180f043f844683fd29e2a68f5c2f9
gr4vytron <33167187+gr4vytr0n@users.noreply.github.com>
2017-11-28 18:19:40 -0800

    tinkering with var_kdd_tree.py

--- classification/knn/kdtree/var_kd_tree.py
+++ classification/knn/kdtree/var_kd_tree.py
@@ -1,13 +1,11 @@
 from numpy import var, array, where
 
+def variances(data):
+    variances = var(data, axis=0)
 
-def kd_tree(data, chk_var=True, var_i=None, cnt=0):
-    if chk_var == True:
-        variances = var(data, axis=0)
-        variance_sort_indices = variances.argsort()
-        var_i = variance_sort_indices
-        chk_var = False
+    return variances.argsort()
 
+def kd_tree(data, var_i, cnt=0):
     try:
         k = len(data[0])
     except IndexError as e:
@@ -21,46 +19,46 @@ def kd_tree(data, chk_var=True, var_i=None, cnt=0):
 
     return {
         'location': sorted_data[median],
-        'left_child': kd_tree(sorted_data[:median], chk_var, var_i, (cnt + 1)),
-        'right_child': kd_tree(sorted_data[(median + 1):], chk_var, var_i, (cnt + 1))
+        'bounds': [sorted_data[0], sorted_data[-1]],
+        'left_child': kd_tree(sorted_data[:median], var_i, (cnt + 1)),
+        'right_child': kd_tree(sorted_data[(median + 1):], var_i, (cnt + 1))
     }
 
+def knn_search_tree(tree, t_node, variance_sort_indices, k):
+    def test_value(t_nodecurrent_node):
+        return t_node < current_node
 
-
-
-# variance search compares columns while searching tree
-
-
-
-def knn_search_tree(tree, test):
-    def distance(test_node, current_node):
-        return (((test_node - current_node)**2).sum(axis=0))**0.5
+    def test_bounds(bounds):
+        
 
     root = tree['location']
     current_best = root
     at_node = tree
-    while True:
-        current_best_distance = distance(test, current_best)
+
+    cnt = 0
+    while cnt < 5:
+        axis = where(variance_sort_indices == (cnt % k))[0][0]
+        current_best_test = test(t_node[axis], current_best[axis])
 
         if at_node['left_child'] == None and at_node['right_child'] == None:
             break
         elif at_node['left_child'] == None:
             at_node = at_node['right_child']
-            if current_best_distance > distance(test, at_node['location']):
+            if test(t_node[axis], at_node['location'][axis]):
                 current_best = at_node['location']
             else:
                 break
         elif at_node['right_child'] == None:
             at_node = at_node['left_child']
-            if current_best_distance > distance(test, at_node['location']):
+            if test(t_node[axis], at_node['location'][axis]):
                 current_best = at_node['location']
             else:
                 break
         else:
             left_child = at_node['left_child']['location']
             right_child = at_node['right_child']['location']
-            left_dist = distance(test, left_child)
-            right_dist = distance(test, right_child)
+            left_dist = t_node[axis] - left_child[axis]
+            right_dist = t_node[axis] - right_child[axis]
 
             # if the left and right distance are equal an infinite loop
             # would be created -- what to do?
@@ -70,7 +68,8 @@ def knn_search_tree(tree, test):
             elif right_dist > left_dist:
                 current_best = left_child
                 at_node = at_node['left_child']
-    
+    cnt += 1
+
     return current_best
 
 
@@ -90,30 +89,28 @@ if __name__ == '__main__':
     ho_ratio = 0.1
     m = data.shape[0]
     test_dataset_size = int(m * ho_ratio)
-
-    training_set = normalized_dataset[test_dataset_size:]
-
-    tree = kd_tree(training_set)
+    training_set= normalized_dataset[test_dataset_size:]
+    variance = variances(training_set)
+    tree = kd_tree(training_set, variance)
 
     count_same = 0
     for i in range(test_dataset_size):
         # find 3 nearest neighbors
         normalized_test_node = (data[:test_dataset_size][i] - min_vals) / ranges
-        search_results = knn_search_tree(tree, normalized_test_node)
-
+        search_results = knn_search_tree(tree, normalized_test_node, variance, len(normalized_test_node))
+        
         from numpy import where
         kd_label = labels[label_indices[where(((training_set[:, 0] == search_results[0]) &
                                         (training_set[:, 1] == search_results[1]) &
                                         (training_set[:, 2] == search_results[2])))[0]]][0]
-        print(kd_label)
-
+        print(search_results)
         from knn import classify0
 
         result = classify0(normalized_test_node, training_set,
                         label_indices[test_dataset_size:], 3)
 
-        print(labels[result])
-        print(labels[label_indices[i]])
+        # print(labels[result])
+        # print(labels[label_indices[i]])
 
         if labels[result] == kd_label:
             count_same += 1
