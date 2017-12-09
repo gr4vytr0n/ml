0519b54a0bce9a6bc61ef23223f9f50b1f965f68
gr4vytr0n <33167187+gr4vytr0n@users.noreply.github.com>
2017-12-08 22:52:24 -0800

    added stochastic gradient ascent code and plot code for logistic_regression.py

--- classification/logistic_regression/logistic_regression.py
+++ classification/logistic_regression/logistic_regression.py
@@ -2,21 +2,22 @@
     logistic regression
 '''
 from math import exp
-from numpy import mat, shape, ones, exp
-
+from random import uniform
+from numpy import mat, shape, ones, exp, array, arange, sum, ndarray
+import matplotlib.pyplot as plt
 
 def load_dataset(filename):
     ''' load data '''
-    dataset = []
-    labels = []
+    d_set = []
+    l_set = []
 
     with open(filename) as file:
         for line in file.readlines():
             line_list = line.strip().split()
-            dataset.append([1.0, float(line_list[0]), float(line_list[1])])
-            labels.append(int(line_list[2]))
-    
-    return dataset, labels
+            d_set.append([1.0, float(line_list[0]), float(line_list[1])])
+            l_set.append(int(line_list[2]))
+
+    return d_set, l_set
 
 
 def sigmoid(in_x):
@@ -41,7 +42,66 @@ def gradient_ascent(d_set, l_set):
 
     return weights
 
-if __name__ == '__main__':
-    dataset, labels = load_dataset('/media/gtron/files/ml/ml/classification/logistic_regression/testSet.txt')
+
+def stochastic_gradient_ascent(d_set, l_set, num_iters=150):
+    ''' stochastic gradient ascent '''
+    m, n = shape(d_set)
+    weights = ones(n)
+
+    for j in range(num_iters):
+        d_idx = list(range(m))
+
+        for i in range(m):
+            alpha = 4 / (1.0 + j + i) + 0.01
+            rand_idx = int(uniform(0, len(d_idx)))
+            h = sigmoid(sum(d_set[rand_idx] * weights))
+            error = l_set[rand_idx] - h
+            weights = weights + alpha * error * d_set[rand_idx]
+            del d_idx[rand_idx]
     
-    print(gradient_ascent(dataset, labels))
+    return weights
+
+
+def plot_best_fit(wei):
+    ''' ploy the decision boundary '''
+    if isinstance(wei, ndarray): 
+        plt_weights = wei
+    else:
+        plt_weights = wei.getA()
+
+    d_set, l_set = load_dataset(
+        '/media/gtron/files/ml/ml/classification/logistic_regression/testSet.txt')
+    dataset_array = array(d_set)
+    n = shape(dataset_array)[0]
+    x_coord_1 = []
+    y_coord_1 = []
+    x_coord_2 = []
+    y_coord_2 = []
+
+    for i in range(n):
+        if int(l_set[i]) == 1:
+            x_coord_1.append(dataset_array[i, 1])
+            y_coord_1.append(dataset_array[i, 2])
+        else:
+            x_coord_2.append(dataset_array[i, 1])
+            y_coord_2.append(dataset_array[i, 2])
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(x_coord_1, y_coord_1, s=30, c='red', marker='s')
+    ax.scatter(x_coord_2, y_coord_2, s=30, c='green')
+    x = arange(-3.0, 3.0, 0.1)
+    y = (-plt_weights[0] - plt_weights[1] * x) / plt_weights[2]
+    ax.plot(x, y)
+    plt.xlabel('X1')
+    plt.ylabel('X2')
+    plt.show()
+
+
+if __name__ == '__main__':
+    dataset, labels = load_dataset(
+        '/media/gtron/files/ml/ml/classification/logistic_regression/testSet.txt')
+
+    weights = stochastic_gradient_ascent(array(dataset), labels)
+    
+    plot_best_fit(weights)
