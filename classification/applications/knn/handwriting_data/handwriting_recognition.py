9959d19df6536e54b49dbe7c63652cf9ac506679
gr4vytr0n <33167187+gr4vytr0n@users.noreply.github.com>
2017-12-15 16:56:31 -0800

    working on refactoring decision trees code

--- classification/applications/knn/handwriting_data/handwriting_recognition.py
+++ classification/applications/knn/handwriting_data/handwriting_recognition.py
@@ -14,20 +14,21 @@ from knn import classify
 
 def build_file_lists(path, dirs):
     ''' insert filenames from given directories into a list '''
+
     save_cwd = getcwd()
     chdir(path)
 
     # test at index 0, training at index 1
     filenames = [listdir(dir) for dir in dirs]
 
-    chdir(path+dirs[0])
+    chdir(path + dirs[0])
     test_files = []
     for number_file in filenames[0]:
         with open(number_file) as n_file:
             test_files.append(n_file.read())
 
-    chdir(path+dirs[1])
-    train_files = []    
+    chdir(path + dirs[1])
+    train_files = []
     for number_file in filenames[1]:
         with open(number_file) as n_file:
             train_files.append(n_file.read())
@@ -38,38 +39,40 @@ def build_file_lists(path, dirs):
 
 
 def to_vectors(data):
+    ''' convert arrays of handwriting into vectors of 1s and 0s '''
+
     vectors = zeros((len(data), 1024))
-    for h in range(len(data)):
-        lines = data[h].split('\n')
+    for idx, value in enumerate(data):
+        lines = value.split('\n')
         vector = zeros((1, 1024))
         for i in range(32):
             line = lines[i]
             for j in range(32):
-                vector[0, 32*i+j] = int(line[j])
-        vectors[h, :] = vector
+                vector[0, 32 * i + j] = int(line[j])
+        vectors[idx, :] = vector
+
     return vectors
 
 
-def classify_handwriting(testData, trainingData, filenames):
-    testLabels = []
-    trainingLabels = []
-    for filename in filenames[0]:
-        label = int(filename.split('.')[0].split('_')[0])
-        testLabels.append(label)
-    for filename in filenames[1]:
-        label = int(filename.split('.')[0].split('_')[0])
-        trainingLabels.append(label)
-    errorCount = 0.0
-    for test in range(len(testData)):
-        classifierResult = classify(testData[test], trainingData, trainingLabels, 3)
-        # print('classifier result: {}, real answer: {}'.format(classifierResult, testLabels[test]))
-        if (classifierResult != testLabels[test]):
-            errorCount += 1.0
-    print('total errors: {}'.format(errorCount))
-    print('total error rate: {}'.format(errorCount/float(len(testData))))
+def classify_handwriting(test_set, train_set, f_names):
+    ''' classifiy test vectors '''
+
+    test_labels = [int(f.split('.')[0].split('_')[0]) for f in f_names[0]]
+
+    train_labels = [int(f.split('.')[0].split('_')[0]) for f in f_names[1]]
+
+    error_count = 0.0
+    for test in range(len(test_set)):
+        classification = classify(test_set[test], train_set, train_labels, 3)
+        # print('classifier result: {}, real answer: {}'.format(classifierResult, test_labels[test]))
+        if (classification != test_labels[test]):
+            error_count += 1.0
+
+    print('total errors: {}'.format(error_count))
+    print('total error rate: {}'.format(error_count/float(len(test_set))))
 
 
-# create arrays of sample vectors for test and training data
+# create arrays of sample vectors for test and training datasets
 file_path = '/media/gtron/files/ml/ml/datasets/hw/'
 file_dirs = ['testDigits', 'trainingDigits']
 test_files, train_files, filenames = build_file_lists(file_path, file_dirs)
--- classification/trees/decision-trees.py
+++ classification/trees/decision-trees.py
@@ -1,3 +1,5 @@
+
+
 from math import log
 import operator
 
