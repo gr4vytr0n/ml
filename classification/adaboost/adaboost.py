14bafae128a6d29f5c9872ad00fb821efce00b01
gr4vytr0n <33167187+gr4vytr0n@users.noreply.github.com>
2018-01-03 13:37:48 -0800

    working on adaboost using decision stumps

--- classification/adaboost/adaboost.py
+++ classification/adaboost/adaboost.py
@@ -2,7 +2,7 @@
     adaboost meta-alogorithm
 '''
 
-from numpy import ones, shape, mat, zeros
+from numpy import ones, shape, mat, zeros, exp, multiply, sign, log
 from math import inf
 
 def stump_classify(d_mat, dimen, thresh_val, thresh_ineq):
@@ -45,8 +45,7 @@ def build_stump(d_arr, labels, D):
                 err_arr = mat(ones((m, 1)))
                 err_arr[predicted_vals == l_mat] = 0
                 weighted_err = D.T * err_arr
-                print('split: dim {}, thresh {}, thresh inequal {}, \
-                  weighted err {}'.format(i, thresh_val, inequal, weighted_err))
+                print('split: dim {}, thresh {}, thresh inequal {}, weighted err {}'.format(i, thresh_val, inequal, weighted_err))
 
                 if weighted_err < min_err:
                     min_err = weighted_err
@@ -57,4 +56,59 @@ def build_stump(d_arr, labels, D):
     
     return best_stump, min_err, best_class_est
 
+def adaboost_trainer_ds(d_arr, labels, num_iter=40):
+    '''
+        adaboost trainer
+    '''
 
+    weak_class_arr = []
+    m = shape(d_arr)[0]
+    D = mat(ones((m, 1)) / m)
+    agg_class_est = mat(zeros((m, 1)))
+
+    for i in range(num_iter):
+        best_stump, err, class_est = build_stump(d_arr, labels, D)
+
+        print('D: {}'.format(D.T))
+
+        alpha = float(0.5 * log((1.0 - err) / max(err, 1e-16)))
+        best_stump['alpha'] = alpha
+        weak_class_arr.append(best_stump)
+        
+        print('class_est: {}'.format(class_est.T))
+
+        expon = multiply(-1 * alpha * mat(labels).T, class_est)
+        D = multiply(D, exp(expon))
+        D = D / D.sum()
+        agg_class_est += alpha * class_est
+
+        print('agg_class_est: {}'.format(agg_class_est.T))
+
+        agg_errs = multiply(sign(agg_class_est) != mat(labels).T, ones((m, 1)))
+        err_rate = agg_errs.sum() / m
+
+        print('total error: {}\n'.format(err_rate))
+
+        if err_rate == 0.0:
+            break
+
+    return weak_class_arr
+
+def adaboost_classify_ds(d_to_class, classifier_arr):
+    '''
+        adaboost classifier
+    '''
+
+    d_mat = mat(d_to_class)
+    m = shape(d_mat)[0]
+    agg_class_est = mat(zeros((m, 1)))
+
+    for i in range(len(classifier_arr)):
+        class_est = stump_classify(d_mat, classifier_arr[i]['dim'], \
+            classifier_arr[i]['thresh'], \
+            classifier_arr[i]['ineq'])
+        agg_class_est += classifier_arr[i]['alpha'] * class_est
+
+        print(agg_class_est)
+    
+    return sign(agg_class_est)
--- classification/applications/adaboost/adaboost_test.py
+++ classification/applications/adaboost/adaboost_test.py
@@ -7,7 +7,7 @@ from sys import path
 from numpy import mat, matrix, ones
 
 path.insert(0, getcwd() + '/classification/adaboost/')
-from adaboost import build_stump
+from adaboost import build_stump, adaboost_trainer_ds, adaboost_classify_ds
 
 
 def simple_dataset():
@@ -33,6 +33,8 @@ def test():
     '''
     d_mat, labels = simple_dataset()
 
-    D = mat(ones((5, 1)) / 5)
+    classifier_arr = adaboost_trainer_ds(d_mat, labels, 9)
 
-    print(build_stump(d_mat, labels, D))
+    print(adaboost_classify_ds([0, 0], classifier_arr))
+    print('\n')
+    print(adaboost_classify_ds([[5, 5], [0, 0]], classifier_arr))
