45be159dadacf69f905f892728c1f4bb3a8ba897
gr4vytr0n <33167187+gr4vytr0n@users.noreply.github.com>
2018-01-02 23:15:28 -0800

    adaboost code simple test working

--- classification/adaboost/adaboost.py
+++ classification/adaboost/adaboost.py
@@ -2,4 +2,59 @@
     adaboost meta-alogorithm
 '''
 
-# code goes here
+from numpy import ones, shape, mat, zeros
+from math import inf
+
+def stump_classify(d_mat, dimen, thresh_val, thresh_ineq):
+    '''
+        descision stump classifier
+    '''
+
+    result_arr = ones((shape(d_mat)[0], 1))
+
+    if thresh_ineq == 'lt':
+        result_arr[d_mat[:, dimen] <= thresh_val] = -1.0
+    else:
+        result_arr[d_mat[:, dimen] > thresh_val] = -1.0
+    
+    return result_arr
+
+def build_stump(d_arr, labels, D):
+    '''
+        build stump
+    '''
+
+    d_mat = mat(d_arr)
+    l_mat = mat(labels).T
+    m, n = shape(d_mat)
+    num_steps = 10.0
+    best_stump = {}
+    best_class_est = mat(zeros((m, 1)))
+    min_err = inf
+
+    for i in range(n):
+        range_min = d_mat[:, i].min()
+        range_max = d_mat[:, i].max()
+
+        step_size = (range_max - range_min) / num_steps
+
+        for j in range(-1, int(num_steps) + 1):
+            for inequal in ['lt', 'gt']:
+                thresh_val = (range_min + float(j) * step_size)
+                predicted_vals = stump_classify(d_mat, i, thresh_val, inequal)
+                err_arr = mat(ones((m, 1)))
+                err_arr[predicted_vals == l_mat] = 0
+                weighted_err = D.T * err_arr
+                print('split: dim {}, thresh {}, thresh inequal {}, \
+                  weighted err {}'.format(i, thresh_val, inequal, weighted_err))
+
+                if weighted_err < min_err:
+                    min_err = weighted_err
+                    best_class_est = predicted_vals.copy()
+                    best_stump['dim'] = i
+                    best_stump['thresh'] = thresh_val
+                    best_stump['ineq'] = inequal
+    
+    return best_stump, min_err, best_class_est
+
+
--- classification/applications/adaboost/adaboost_test.py
+++ classification/applications/adaboost/adaboost_test.py
@@ -2,4 +2,37 @@
     test script for adaboost algorithm
 '''
 
-# code goes here
+from os import getcwd
+from sys import path
+from numpy import mat, matrix, ones
+
+path.insert(0, getcwd() + '/classification/adaboost/')
+from adaboost import build_stump
+
+
+def simple_dataset():
+    '''
+        returns simple dataset and labels
+    '''
+
+    dset_mat = matrix([
+        [1., 2.1],
+        [2., 1.1],
+        [1.3, 1.],
+        [1., 1.],
+        [2., 1.]
+    ])
+
+    labels = [1.0, 1.0, -1.0, -1.0, 1.0]
+
+    return dset_mat, labels
+
+def test():
+    '''
+        run test script
+    '''
+    d_mat, labels = simple_dataset()
+
+    D = mat(ones((5, 1)) / 5)
+
+    build_stump(d_mat, labels, D)
